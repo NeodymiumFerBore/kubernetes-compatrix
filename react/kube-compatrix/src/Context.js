a81d2e5bd40475dfd130599efa482ee7d62254e9
NeodymiumFerBore <32781483+NeodymiumFerBore@users.noreply.github.com>
2024-02-11 01:33:06 +0100

    Add selectedKubeVersionsContext

--- react/kube-compatrix/src/Context.js
+++ react/kube-compatrix/src/Context.js
@@ -2,3 +2,4 @@ import { createContext } from "react";
 
 // https://www.youtube.com/watch?v=MCTB_w0Guso
 export const countContext = createContext(null);
+export const selectedKubeVersionsContext = createContext(null);
